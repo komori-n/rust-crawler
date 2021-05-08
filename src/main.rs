fcd52fa0ea5ea89eb3ea7d7e308ee69ea8799f24
komori-n <ikamat.kmr@gmail.com>
2021-05-08 13:54:10 +0900

    feat: add a commandline argument to specify an url

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+#[macro_use]
+extern crate clap;
+
 use anyhow::Result;
+use clap::{App, Arg};
 use reqwest::blocking::ClientBuilder;
 use rust_crawler::LinkExtractor;
 use url::Url;
 
 fn main() -> Result<()> {
-    let url = Url::parse("https://www.rust-lang.org")?;
+    let matches = app_from_crate!()
+        .arg(
+            Arg::with_name("URL")
+                .default_value("https://www.rust-lang.org")
+                .index(1),
+        )
+        .get_matches();
+
+    let url = Url::parse(
+        matches
+            .value_of("URL")
+            .unwrap_or("https://www.rust-lang.org"),
+    )?;
     let client = ClientBuilder::new().build()?;
     let extractor = LinkExtractor::from_client(client);
 
