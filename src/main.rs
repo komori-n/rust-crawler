e656f45f2b166df8de431cb77294fbe008c8bbd3
komori-n <ikamat.kmr@gmail.com>
2021-05-08 10:15:05 +0900

    feat: collect hyperlinks in a page

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use anyhow::Result;
+use select::document::Document;
+use select::predicate::Name;
+use url::ParseError as UrlParseError;
+use url::Url;
 
 fn main() -> Result<()> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
-
-    println!("body = {:?}", body);
+    let response = reqwest::blocking::get("https://www.rust-lang.org")?;
+    let base_url = response.url().clone();
+    let body = response.text()?;
+    let doc = Document::from(body.as_str());
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        match Url::parse(href) {
+            Ok(url) => println!("{}", url),
+            Err(UrlParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                println!("{}", url);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+        println!("{:?}", Url::parse(href));
+    }
 
     Ok(())
 }
