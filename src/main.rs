661e610371c898b4ea1579f72c08916ea7e787ae
komori-n <ikamat.kmr@gmail.com>
2021-05-08 10:25:13 +0900

    refactor: move parsing codes from main.rs into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
 use anyhow::Result;
-use select::document::Document;
-use select::predicate::Name;
-use url::ParseError as UrlParseError;
+use reqwest::blocking::ClientBuilder;
+use rust_crawler::LinkExtractor;
 use url::Url;
 
 fn main() -> Result<()> {
-    let response = reqwest::blocking::get("https://www.rust-lang.org")?;
-    let base_url = response.url().clone();
-    let body = response.text()?;
-    let doc = Document::from(body.as_str());
-    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
-        match Url::parse(href) {
-            Ok(url) => println!("{}", url),
-            Err(UrlParseError::RelativeUrlWithoutBase) => {
-                let url = base_url.join(href)?;
-                println!("{}", url);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-        println!("{:?}", Url::parse(href));
+    let url = Url::parse("https://www.rust-lang.org")?;
+    let client = ClientBuilder::new().build()?;
+    let extractor = LinkExtractor::from_client(client);
+
+    let links = extractor.get_links(url)?;
+    for link in links.iter() {
+        println!("{}", link);
     }
 
     Ok(())
